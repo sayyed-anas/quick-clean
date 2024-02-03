edc53bf6468d12054a24b9c89dc73e36671501a6
sayyed-anas <sayyedanas142@gmail.com>
2024-02-03 19:22:08 +0530

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+
+const viewsPath = path.join(__dirname, "/resources/views");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// set Template engines
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`listning on Porn no :- ${PORT}`);
+});
