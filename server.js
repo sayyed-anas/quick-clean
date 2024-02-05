157a9a4234ec23c49f87b21c32ff2aa4562d38f9
sayyed-anas <sayyedanas142@gmail.com>
2024-02-05 19:57:53 +0530

    make home page

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const viewsPath = path.join(__dirname, "/resources/views");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
