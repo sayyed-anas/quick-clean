a56596bbc18fe0686f60d21ca2ab772edbeac71a
sayyed-anas <sayyedanas142@gmail.com>
2024-02-06 20:04:24 +0530

    auth and add cart

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 
 const viewsPath = path.join(__dirname, "/resources/views");
 
@@ -9,13 +10,26 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+// set Template engines
+app.use(expressLayout);
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// set Template engines
-app.set("views", viewsPath);
-app.set("view engine", "ejs");
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/Register");
+});
 
 app.listen(PORT, () => {
   console.log(`listning on Porn no :- ${PORT}`);
